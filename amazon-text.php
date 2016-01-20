8a990fb5a907ed7a6e0d3afcbbff33197d54586a
Jacob Findley <jfindley2@cnm.edu>
2016-01-20 09:53:39 -0700

    Added amazon-text

---
+++ amazon-text.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Amazon Product Entities, Persona, and Use Case</title>
+	</head>
+	<body>
+		<h1>Entities</h1>
+		<p>m profiles give n Reviews.
+			1 Product receives n Reviews
+
+			1 Review receives m Helpful Votes.
+			m Profiles give m Helpful Votes.
+
+			m Profiles give n Comments.
+			1 Review receives n Comments.
+
+			m profiles ask n Questions.
+			1 Product hosts n Questions
+
+			m Profiles respond with n answers.
+			1 Question receives n answers.
+
+		</p>
+		<h1>Persona</h1>
+		<p>Penelope Lane, age 17. A senior high school student at the Zann School of Music who is currently planning on attending Miskatonic U. She is ambitious and, perhaps, studies too hard, but she does find time to socialise. One friend and three classmates with whom she has a friendly albeit distant acquaintanceship are planning on going to the same school. She is moderately exasperated with her parents who, she thinks, are trying too hard to help and being helicopter parents. She is proud of her skill with the violin. She has no siblings, though always wanted one.
+		</p>
+		<h1>Use Case</h1>
+		<p>Penelope is browsing Amazon in search for a laptop computer, focused primarily on Windows. Her parents have, as a graduation gift, decided to buy her a laptop, and right now she is looking through the reviews of one such model to determine whether or not it is worth the price. She wishes to find a laptop that is both technologically stable and well liked by its reviewers, bookmarking the URLs of those that seem promising for further comparison.
+
+			Penelope opens the Amazon home page.
+			Penelope starts to type, “Computer,” in the Amazon search bar.
+			Amazon autofills the word she wishes to type, and gives suggestions (Which are links).
+			Penelope chooses one of the suggestions, and clicks on it.
+			Amazon takes her to the page of the product she chose.
+			The Amazon page shows her the product’s name, description, picture, reviews, price, Star reviews, and technical information.
+			Penelope reviews the information Amazon provides, and compares it to what she wants.
+			Penelope focuses on the reviews, focusing on those that have high Helpful votes.
+
+		</p>
+
+	</body>
+</html>
