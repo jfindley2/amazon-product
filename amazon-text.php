b2cbc1c9bc7974c0a71a868a02eeea27244cdc52
Jacob Findley <jfindley2@cnm.edu>
2016-01-20 09:55:16 -0700

    Added amazon-text

--- amazon-text.php
+++ amazon-text.php
@@ -6,36 +6,36 @@
 	</head>
 	<body>
 		<h1>Entities</h1>
-		<p>m profiles give n Reviews.
-			1 Product receives n Reviews
+		<p><br/>m profiles give n Reviews.<br/>
+			1 Product receives n Reviews<br/>
 
-			1 Review receives m Helpful Votes.
-			m Profiles give m Helpful Votes.
+			1 Review receives m Helpful Votes.<br/>
+			m Profiles give m Helpful Votes.<br/>
 
-			m Profiles give n Comments.
-			1 Review receives n Comments.
+			m Profiles give n Comments.<br/>
+			1 Review receives n Comments.<br/>
 
-			m profiles ask n Questions.
-			1 Product hosts n Questions
+			m profiles ask n Questions.<br/>
+			1 Product hosts n Questions<br/>
 
-			m Profiles respond with n answers.
-			1 Question receives n answers.
+			m Profiles respond with n answers.<br/>
+			1 Question receives n answers.<br/><br/>
 
 		</p>
 		<h1>Persona</h1>
-		<p>Penelope Lane, age 17. A senior high school student at the Zann School of Music who is currently planning on attending Miskatonic U. She is ambitious and, perhaps, studies too hard, but she does find time to socialise. One friend and three classmates with whom she has a friendly albeit distant acquaintanceship are planning on going to the same school. She is moderately exasperated with her parents who, she thinks, are trying too hard to help and being helicopter parents. She is proud of her skill with the violin. She has no siblings, though always wanted one.
+		<p><br/>Penelope Lane, age 17. A senior high school student at the Zann School of Music who is currently planning on attending Miskatonic U. She is ambitious and, perhaps, studies too hard, but she does find time to socialise. One friend and three classmates with whom she has a friendly albeit distant acquaintanceship are planning on going to the same school. She is moderately exasperated with her parents who, she thinks, are trying too hard to help and being helicopter parents. She is proud of her skill with the violin. She has no siblings, though always wanted one.<br/>
 		</p>
 		<h1>Use Case</h1>
-		<p>Penelope is browsing Amazon in search for a laptop computer, focused primarily on Windows. Her parents have, as a graduation gift, decided to buy her a laptop, and right now she is looking through the reviews of one such model to determine whether or not it is worth the price. She wishes to find a laptop that is both technologically stable and well liked by its reviewers, bookmarking the URLs of those that seem promising for further comparison.
+		<p><br/>Penelope is browsing Amazon in search for a laptop computer, focused primarily on Windows. Her parents have, as a graduation gift, decided to buy her a laptop, and right now she is looking through the reviews of one such model to determine whether or not it is worth the price. She wishes to find a laptop that is both technologically stable and well liked by its reviewers, bookmarking the URLs of those that seem promising for further comparison.<br/><br/>
 
-			Penelope opens the Amazon home page.
-			Penelope starts to type, “Computer,” in the Amazon search bar.
-			Amazon autofills the word she wishes to type, and gives suggestions (Which are links).
-			Penelope chooses one of the suggestions, and clicks on it.
-			Amazon takes her to the page of the product she chose.
-			The Amazon page shows her the product’s name, description, picture, reviews, price, Star reviews, and technical information.
-			Penelope reviews the information Amazon provides, and compares it to what she wants.
-			Penelope focuses on the reviews, focusing on those that have high Helpful votes.
+			Penelope opens the Amazon home page.<br/>
+			Penelope starts to type, “Computer,” in the Amazon search bar.<br/>
+			Amazon autofills the word she wishes to type, and gives suggestions (Which are links).<br/>
+			Penelope chooses one of the suggestions, and clicks on it.<br/>
+			Amazon takes her to the page of the product she chose.<br/>
+			The Amazon page shows her the product’s name, description, picture, reviews, price, Star reviews, and technical information.<br/>
+			Penelope reviews the information Amazon provides, and compares it to what she wants.<br/>
+			Penelope focuses on the reviews, focusing on those that have high Helpful votes.<br/>
 
 		</p>
 
