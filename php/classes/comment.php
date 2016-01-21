58a71c51708f2257977fb35896086464252c3c49
Jacob Findley <jfindley2@cnm.edu>
2016-01-21 11:15:40 -0700

    Added comment.php

---
+++ php/classes/comment.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Class Comment
+ *
+ * @author Jacob Findley <jfindley2@cnm.edu>
+ */
+
+class Comment {
+	/**
+	 * id for Comment; this is the primary key
+	 * @var int $commentId
+	 */
+	private $commentId;
+	/**
+	 *
+	 * @var int $profileId
+	 */
+	private $profileId;
+}
