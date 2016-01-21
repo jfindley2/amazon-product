ea324e2bf05c0cb63e086ef2ee0ae33bbf5ebbfa
Jacob Findley <jfindley2@cnm.edu>
2016-01-21 09:33:08 -0700

    Added profile.php

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -29,6 +29,97 @@ class Profile {
 	private $blurb;
 
 	/**
-	 * Add
+	 * Accessor method for $profileId
+	 * @return int value of $profileId
 	 */
+	public function getProfileId() {
+		return $this->profileId;
+	}
+	/**
+	 * mutator method for $profileId
+	 *
+	 * @param int $newProfileId
+	 * @throws UnexpectedValueException if $newProfileId is not a proper int
+	 * @throws RangeException if $newProfileId is not positive
+	 */
+	public function setProfileId($newProfileId) {
+		$newProfileId = filter_var($newProfileId, FILTER_VALIDATE_INT);
+		//if filter_var rejects the variable, throw an exception
+		if($newProfileId === false) {
+			throw(new UnexpectedValueException("Profile Id is not a proper int"));
+		}
+		//If $newProfileId is out of range, throw an exception.
+		if($newProfileId <= 0) {
+			throw(new RangeException("Profile ID must be positive"));
+		}
+		$this->profileId = $newProfileId;
+	}
+
+	/**
+	 * Accessor method for $name
+	 * @return string value of $name
+	 */
+	public function getName() {
+		return $this->name;
+	}
+	/**
+	 * Mutator method for $name
+	 *
+	 * @param string $newName
+	 * @throws UnexpectedValueException if $newName is not a proper string
+	 */
+	public function setName($newName) {
+		//Verify the data is of the right type
+		$newName = trim($newName);
+		$newName = filter_var($newName, FILTER_SANITIZE_STRING);
+		if (null($newName) === true) {
+			throw(new UnexpectedValueException("Content is empty or insecure"));
+		}
+		$this->name = $newName;
+	}
+	/**
+	 * Accessor method for $location
+	 * @return string value of $location
+	 */
+	public function getLocation() {
+		return $this->location;
+	}
+	/**
+	 * Mutator method for $location
+	 *
+	 * @param string $newLocation
+	 * @throws UnexpectedValueException if $newLocation is not a proper string
+	 */
+	public function setLocation($newLocation) {
+		//Verify the data is of the right type
+		$newLocation = trim($newLocation);
+		$newLocation = filter_var($newLocation, FILTER_SANITIZE_STRING);
+		if (null($newLocation) === true) {
+			throw(new UnexpectedValueException("Content is empty or insecure"));
+		}
+		$this->location = $newLocation;
+	}
+
+	/**
+	 * Accessor method for $blurb
+	 * @return string value of $blurb
+	 */
+	public function getBlurb() {
+		return $this->blurb;
+	}
+	/**
+	 * Mutator method for $blurb
+	 *
+	 * @param string $newBlurb
+	 * @throws UnexpectedValueException if $newBlurb is not a proper string
+	 */
+	public function setBlurb($newBlurb) {
+		//Verify the data is of the right type
+		$newBlurb = trim($newBlurb);
+		$newBlurb = filter_var($newBlurb, FILTER_SANITIZE_STRING);
+		if (null($newBlurb) === true) {
+			throw(new UnexpectedValueException("Content is empty or insecure"));
+		}
+		$this->blurb = $newBlurb;
+	}
 }
