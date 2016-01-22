36275006d6d1d650948a70b2368124a629cf85f1
Jacob Findley <jfindley2@cnm.edu>
2016-01-22 11:58:16 -0700

    Added product-shakedown

--- php/classes/product-shakedown.php
+++ php/classes/product-shakedown.php
@@ -4,3 +4,8 @@ require_once("product.php");
 $pdo = connectToEncryptedMySQL("/etc/apache2/data-design/jfindley2.ini");
 
 
+$product = new Product(null, "imagefile", 10, "Info", "Detail", "Tech", "Name");
+$product->insert($pdo);
+$product->setProductName("This is the new name");
+$product->update($pdo);
+$product->delete($pdo);
